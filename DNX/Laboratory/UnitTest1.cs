74a21db503b7f22a715d456094ccad8bd6c1a75b
radiospace <radiospace83@gmail.com>
2015-06-10 06:12:23 -0700

    getting started !

---
+++ DNX/Laboratory/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Laboratory
+{
+    [TestClass]
+    public class drc_games_tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DNX/Shell/ShellForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shell
+{
+    public partial class ShellForm : Form
+    {
+        Point centerScreen, oldMousePos;
+
+        public ShellForm()
+        {
+            InitializeComponent();
+
+            ClientSize = new System.Drawing.Size(800, 600);
+            centerScreen = PointToClient(new Point(400, 300));
+            oldMousePos = centerScreen;
+
+            Cursor.Position = centerScreen;
+            
+            Cursor.Hide();
+
+            
+            
+        }
+    }
+}
